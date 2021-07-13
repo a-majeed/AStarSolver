26322f1f678ce4347605b1694412c7280e5abe97
Azaan Majeed <azaan@ualberta.ca>
2021-07-13 14:32:04 -0600

    added time outcome states

--- bearmaps/hw4/AStarSolver.java
+++ bearmaps/hw4/AStarSolver.java
@@ -1,6 +1,6 @@
 package bearmaps.hw4;
 import bearmaps.proj2ab.ArrayHeapMinPQ;
-
+import java.util.*;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -27,5 +27,36 @@ public ASTarSolver(AStarGraph<Vertex> input, Vertex start, Vertex end, double ti
     numStatesExplored = 0;
     solutionWeight = 0;
     timeSpent = 0;
+
+
+    fringes.add(start, input.estimatedDistancetoGoal(start,end));
+    distTo.put(start, 0.0);
+
+    long startTime = system.currentTimeMillis();
+
+    while (fringes.size() != 0){
+
+        Vertex p = fringes.removeSmallest();
+        timeSpent = (System.currentTimeMillis() - startTime)/1000.0;
+        numStatesExplored++;
+
+        if (p.equal(end)){
+            outcome = SolverOutcome.SOLVED;
+            updateSolution(start, end);
+            solutionWeight = distTo.get(end);
+            return;
+
+        }
+
+        if (timeSpent > timeout){
+            outcome = SolverOutcome.TIMEOUT;
+            return;
+        }
+
+        // need to relax outgoing edges
+
+
+    }
+
 
 }
